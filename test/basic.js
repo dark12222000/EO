0b08b50e9e0683892a476a9ce264ea6d7dfd7757
Derrick Hinkle <derrick@pristine.io>
2015-03-11 18:47:09 -0500

    cache!

--- test/basic.js
+++ test/basic.js
@@ -1,5 +1,8 @@
 var EO = require('../lib/eo.js')({email: 'grimd.666+eve@gmail.com'});
 var myChar = EO.character({keyID: '4189233', vCode: 'gNeHij6pXeSRPWsvVhd4THm015g6UAmNhYDOVpYitMgx2If7sK6SdgPDg9FfxxPB'});
-myChar.fetchEndpoint('/account/APIKeyInfo.xml.aspx', function(body){
-  console.log(body);
+myChar.fetchEndpoint('/account/APIKeyInfo.xml.aspx', function(err, result){
+  console.log('No Cache', result);
+  myChar.fetchEndpoint('/account/APIKeyInfo.xml.aspx', function(err, result){
+    console.log('Should Cache', result);
+  });
 });
